7e526036f9e3a3ceac609479e011d5e09c5d8c56
Aiden Shih <Aiden@Aiden-2.local>
2018-02-07 12:33:47 -0500

    layout changes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,11 +4,7 @@ import axios from "axios"
 import './App.css'
 import NewUser from "./users/NewUser"
 import LoginUser from "./users/LoginUser"
-<<<<<<< HEAD:frontend/src/components/App.js
-import Game from "./game/Game"
-=======
 import ZombieTakeover from "./game/ZombieTakeover"
->>>>>>> 7fe182ec73bab28c84cfbf066506d2559ae9f5d6:frontend/src/App.js
 import LogOut from "./users/LogOut"
 
 class App extends React.Component {
@@ -41,11 +37,7 @@ class App extends React.Component {
     if (!user) {
       return <LoginUser setUser={this.setUser} />
     }
-<<<<<<< HEAD:frontend/src/components/App.js
-    return <Game id={user.username} />
-=======
     return <ZombieTakeover id={user.username} />
->>>>>>> 7fe182ec73bab28c84cfbf066506d2559ae9f5d6:frontend/src/App.js
   }
 
   renderLogOut = () => {
@@ -56,17 +48,13 @@ class App extends React.Component {
     const { user, newUser } = this.state
     return (
       <div className="App">
-        <div>
+        <div className='appHeader'>
           <h1>Zombie Takeover</h1>
         </div>
 
         <Route exact path="/" component={LoginUser} />
         <Route path="/users/new" component={NewUser} />
-<<<<<<< HEAD:frontend/src/components/App.js
-        <Route path="/users/AntTakeover" component={Game} />
-=======
         <Route path="/users/ZombieTakeover" component={ZombieTakeover} />
->>>>>>> 7fe182ec73bab28c84cfbf066506d2559ae9f5d6:frontend/src/App.js
         <Route exact path="/users/login" render={this.renderLogin} />
         <Route path="/users/logout" render={this.renderLogOut} />
       </div>
